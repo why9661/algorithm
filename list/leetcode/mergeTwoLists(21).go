008080156c384ebb6de790306458d96435eeab94
why <hy_world@126.com>
2021-09-14 10:18:10 +0800

    list

--- list/leetcode/mergeTwoLists(21).go
+++ list/leetcode/mergeTwoLists(21).go
@@ -1,5 +1,13 @@
 package leetcode
 
+/*
+合并两个有序链表
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的
+
+解：
+头结点的使用
+*/
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
--- list/leetcode/removeNthFromEnd(19).go
+++ list/leetcode/removeNthFromEnd(19).go
@@ -1,6 +1,15 @@
 package leetcode
 
-// 使用快慢指针
+/*
+删除链表的倒数第N个节点，并且返回链表的第一个节点
+
+解：
+此题中的head表示链表的第一个节点，而非头结点
+主要问题是如何找到倒数第n个节点（的前一个结点）？
+1、循环一次得到链表长度，再循环找到目标结点
+2、快慢指针
+头结点（哨兵节点）：可以方便删除第一个元素。
+*/
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	result := &ListNode{}
 	result.Next = head
