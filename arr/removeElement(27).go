385bb2be9f22d5d8d98c55f0006988035ab872a1
why <hy_world@126.com>
2021-09-28 10:30:46 +0800

    移除元素

---
+++ arr/removeElement(27).go
@@ -0,0 +1,13 @@
+package arr
+
+func removeElement(nums []int, val int) int {
+	res := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[res] = nums[i]
+			res++
+		}
+	}
+
+	return res
+}
