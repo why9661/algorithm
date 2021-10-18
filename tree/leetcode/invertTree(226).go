911fd741659ef28a45269624d3c9aa927c67cc3b
why <hy_world@126.com>
2021-10-18 22:36:56 +0800

    翻转二叉树

---
+++ tree/leetcode/invertTree(226).go
@@ -0,0 +1,14 @@
+package leetcode
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	left := invertTree(root.Left)
+	right := invertTree(root.Right)
+	root.Left = right
+	root.Right = left
+
+	return root
+}
