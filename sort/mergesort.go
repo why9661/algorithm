1b4ad521a633c23ec80ed090770c14089a2bc566
why <wanghaoyu@limayao.com>
2022-06-01 13:50:58 +0800

    mergesort

--- sort/mergesort.go
+++ sort/mergesort.go
@@ -17,6 +17,7 @@ O(n)
 
 func MergeSort(arr []int) []int {
 	length := len(arr)
+	// baseline
 	if length < 2 {
 		return arr
 	}
@@ -48,6 +49,47 @@ func merge(arr1 []int, arr2 []int) []int {
 
 	if len(arr2) != 0 {
 		result = append(result, arr2...)
+	}
+
+	return result
+}
+
+func MergeSortV2(arr []int) []int {
+	// 先split然后merge
+	length := len(arr)
+	// baseline
+	if length < 2 {
+		return arr
+	}
+
+	mid := length / 2
+
+	left := MergeSortV2(arr[:mid])
+	right := MergeSortV2(arr[mid:])
+
+	return mergeV2(left, right)
+}
+
+func mergeV2(a, b []int) []int {
+	var result []int
+	i, j := 0, 0
+
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			result = append(result, a[i])
+			i++
+		} else {
+			result = append(result, b[j])
+			j++
+		}
+	}
+
+	if i < len(a) {
+		result = append(result, a[i:]...)
+	}
+
+	if j < len(b) {
+		result = append(result, b[j:]...)
 	}
 
 	return result
